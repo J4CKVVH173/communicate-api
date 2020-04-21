0ac4d009f924f31b1f02eb936c8efe379228f9f2
Andrey <i78901234567890@gmail.com>
2020-04-21 18:37:48 +0300

    update. Добавлена корневая точка входа в покет. Обновлены ignor файлы

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict"
+// файл используется как точка входа при искользовании проекта как npm пакета
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _communication = _interopRequireDefault(require("./dist"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = _communication["default"];
+exports["default"] = _default;
