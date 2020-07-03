618d5da6e8845250519a1c81aa1d3ac96ddc522f
Andrey <i78901234567890@gmail.com>
2020-07-03 11:58:03 +0300

    update. Changed default developerUrl from localhost to 127.0.0.1 update. Added withCredentials prop to config

--- src/communicate.ts
+++ src/communicate.ts
@@ -1,57 +1,58 @@
-import Utils from "./utils";
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import Utils from './utils';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 interface ICommunicate {
-    session: AxiosInstance,
+  session: AxiosInstance;
 }
 
-export interface IUserConfig extends AxiosRequestConfig{
-    devUrl?: string,
-    prodUrl?: string
+export interface IUserConfig extends AxiosRequestConfig {
+  devUrl?: string;
+  prodUrl?: string;
 }
-
 
 export default class Communicate implements ICommunicate {
-    private defaultHeaders = {
-        'Content-Type': 'application/json'
-    }
+  private defaultHeaders = {
+    'Content-Type': 'application/json',
+  };
 
-    private defaultConfig = {
-        xsrfCookieName: 'csrftoken',
-        xsrfHeaderName: 'X-CSRFToken',
-        timeout: 100000,
-    }
+  private defaultConfig = {
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
+    timeout: 100000,
+    withCredentials: true,
+  };
 
-    private utils = new Utils();
-    session: AxiosInstance;
+  private utils = new Utils();
+  session: AxiosInstance;
 
-    constructor(userConfig?: IUserConfig) {
-        const {devUrl, prodUrl, headers} = {...userConfig};
-        const baseURL = this.definitionBaseUrl(devUrl, prodUrl);
-        // preparation headers to argument of function
-        const resultHeaders = {...this.defaultHeaders, ...headers};
-        // It's important to save spread order. First the default settings are unpacked and then customs.
-        // This is done for a higher priority of custom settings. header arg must be last, bcs userConfig
-        // may contain header arg too and userConfig args can rewrite default headers
-        this.session = axios.create({
-            baseURL,
-            ...this.defaultConfig, ...userConfig,
-            headers: resultHeaders,
-        });
-    }
+  constructor(userConfig?: IUserConfig) {
+    const { devUrl, prodUrl, headers } = { ...userConfig };
+    const baseURL = this.definitionBaseUrl(devUrl, prodUrl);
+    // preparation headers to argument of function
+    const resultHeaders = { ...this.defaultHeaders, ...headers };
+    // It's important to save spread order. First the default settings are unpacked and then customs.
+    // This is done for a higher priority of custom settings. header arg must be last, bcs userConfig
+    // may contain header arg too and userConfig args can rewrite default headers
+    this.session = axios.create({
+      baseURL,
+      ...this.defaultConfig,
+      ...userConfig,
+      headers: resultHeaders,
+    });
+  }
 
-    /**
-     * Method for determining the base url for develop and prod machine.
-     * @param dev - url for develop passed like custom config
-     * @param prod - url for production passed like custom config
-     */
-    private definitionBaseUrl(dev?: string, prod?: string) {
-        let baseUrl;
-        if (process.env.NODE_ENV === 'production') {
-            baseUrl = prod || this.utils.productionUrl;
-        } else {
-            baseUrl = dev || this.utils.developerUrl;
-        }
-        return baseUrl;
+  /**
+   * Method for determining the base url for develop and prod machine.
+   * @param dev - url for develop passed like custom config
+   * @param prod - url for production passed like custom config
+   */
+  private definitionBaseUrl(dev?: string, prod?: string) {
+    let baseUrl;
+    if (process.env.NODE_ENV === 'production') {
+      baseUrl = prod || this.utils.productionUrl;
+    } else {
+      baseUrl = dev || this.utils.developerUrl;
     }
+    return baseUrl;
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,15 @@
  * Class with helpful methods to configure the session.
  */
 export default class Utils {
+  /**
+   * Default baseURL for request on dev machine.
+   */
+  developerUrl = 'http://127.0.0.1:8000/';
 
-    /**
-     * Default baseURL for request on dev machine.
-     */
-    developerUrl = 'http://localhost:8000';
-
-    /**
-     * Getter for default baseURL on production machine.
-     */
-    get productionUrl(): string {
-        return window.location.protocol + '//' + window.location.host;
-    }
+  /**
+   * Getter for default baseURL on production machine.
+   */
+  get productionUrl(): string {
+    return window.location.protocol + '//' + window.location.host;
+  }
 }
